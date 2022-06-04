59b7ac1566957a6384ea78ed3952c9b52196c97b
Kirya <kiril@qw>
2022-06-04 14:24:07 +0300

    123

--- MainForm.py
+++ MainForm.py
@@ -15,14 +15,16 @@ class MyMainForm(QtWidgets.QMainWindow):
         self.__ui.action_20.triggered.connect(self.signs_of_pripen)
         self.__ui.lineEditEnterText.textChanged.connect(self.onEnterText)
         self.dict=['']
+        self.allSymbols = 0
+        self.allTrueSymbols = 0
         self.timerSec = QtCore.QTimer()
         self.timerSec.timeout.connect(self.onTimer)
-        self.reset()
 
     def Start(self):
         self.__ui.lineEditOutputText.setText('')
         self.__ui.lineEditEnterText.setText('')
         self.onEnterText('')
+        self.secValue = 3
         self.timerSec.start(1000)
 
     def keyPressEvent(self, event):
@@ -31,31 +33,46 @@ class MyMainForm(QtWidgets.QMainWindow):
             event.accept()
 
     def onEnterText(self, newText):
+        if newText != '':
+            self.allSymbols += 1
+            a = newText[len(newText)-1]
+            b = self.__ui.lineEditOutputText.text()[len(newText)-1]
+            if a != b:
+                newText = newText.removesuffix(a)
+                self.__ui.lineEditEnterText.setText(newText)
+            else:
+                self.allTrueSymbols += 1
          #self.__ui.lineEditOutputText.setText(newText)
-         if self.__ui.lineEditEnterText.text() == self.__ui.lineEditOutputText.text():
-             #print('y')
-             idx = random.randrange(0, len(self.dict))
-             self.__ui.lineEditOutputText.setText(self.dict[idx])
-             del self.dict[idx]
-             self.__ui.lineEditEnterText.setText('')
+        if self.__ui.lineEditEnterText.text() == self.__ui.lineEditOutputText.text():
+            #print('y')
+            if (len(self.dict) > 0):
+                idx = random.randrange(0, len(self.dict))
+                self.__ui.lineEditEnterText.setText('')
+                self.__ui.lineEditOutputText.setText(self.dict[idx])
+                del self.dict[idx]
 
     def sh_w(self):
-        self.dict=["Год","Дон","Облако","Солнце","Компьютер", "Золото", "Медведь", "Гараж", "Закон", "Город", "Банан", "Туман"]
+        self.dict=["год","дон","облако","солнце","компьютер", "золото", "медведь", "гараж", "закон", "город", "банан",
+                   "туман", "жабры", "жаба","йога","ивняк","йогурт","кабак","лабаз","лава","лаваш","арбат",
+                   "линзы","халат","цапля","чада","шаг","юг","ювелир","юбка","явка","аббат","абажур","абзац","багет",
+                   "багор","багги","база","бадья"]
         #while self.dict:
 
 
     def lon_w(self):
-        self.dict=["Клавиатура","Мышка","Облако","Солнце","Рапира","Расправа","Радар","Рапорт","Разрыв","Разбор","Размер","Разгром","Растирание"
-                   "Ротатор","Радиатор","Разработка","Рубероид","Разоружение","Разнообразие","Равноправие","Разодрать","Раздробить"]
+        self.dict=["клавиатура","кышка","облако","солнце","рапира","расправа","радар","рапорт","разрыв","разбор",
+                   "размер","разгром","растирание","ротатор","радиатор","разработка","рубероид","разоружение",
+                   "разнообразие","равноправие","разодрать","раздробить"]
 
     def onTimer(self):
         self.__ui.lineEdit_2.setText(str(self.secValue))
-        self.secValue-=1
-        if self.secValue ==-1:
+        self.secValue -= 1
+        if self.secValue == -1:
             self.timerSec.stop()
             msgBox = QtWidgets.QMessageBox()
             msgBox.setIcon(QtWidgets.QMessageBox.Information)
-            msgBox.setText("Время вышло. Попробовать снова?")
+            msgBox.setText("Натыкано правильных знаков: "+str(self.allTrueSymbols)+"\n"+
+                           "Натыкано не правильных знаков: "+str(self.allSymbols)+"\n")
             msgBox.setWindowTitle("Время вышло")
             msgBox.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
 
@@ -63,21 +80,23 @@ class MyMainForm(QtWidgets.QMainWindow):
             if returnValue == QtWidgets.QMessageBox.Yes:
                 self.reset()
             else:
-                self.__menuForm.setVisible(True)
-                self.close()
+                self.__ui.lineEditEnterText.setText("")
+                self.__ui.lineEditOutputText.setText("")
+
 
     def reset(self):
-        self.secValue = 60
+        self.Start()
         #self.timerSec.start(1000)
 
     def Exam(self):
-        self.dict = ["Год","Дон","Облако","Солнце","Компьютер", "Золото", "Медведь", "Гараж", "Закон", "Город", "Банан", "Туман",
-                     "Клавиатура","Мышка","Облако","Солнце","Рапира","Расправа","Радар","Рапорт","Разрыв","Разбор","Размер","Разгром","Растирание",
-                     "Ротатор", "Радиатор", "Разработка", "Рубероид", "Разоружение", "Разнообразие", "Равноправие",
-                     "Разодрать", "Раздробить"]
+        self.dict = ["год","дон","облако","солнце","компьютер", "золото", "медведь", "гараж", "закон", "город", "банан", "туман",
+                     "клавиатура","мышка","облако","солнце","рапира","расправа","радар","рапорт","разрыв","разбор","размер","разгром","растирание",
+                     "ротатор", "радиатор", "разработка", "рубероид", "разоружение", "разнообразие", "равноправие",
+                     "разодрать", "раздробить"]
 
     def signs_of_pripen(self):
-        self.dict = [",,,",",,,...","..",".,","..,,", "я,", "ты,", "он,", "она."]
+        self.dict = [",,,",",,,...","..",".,","..,,", "я,", "ты,", "он,", "она.", "А.С. Пушкин и др,.", "и","пр.,","Сон,","бал,","Файл,","зуб и т. п.,", "и т.","в.","т.",
+                     ",,;,;",";,;,;","а,","б;","в,","Г;",",;,;,",";,,;","да,","Ага;","моря;"]
 
 
 
