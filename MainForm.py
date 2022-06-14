e434c56561f2bd594a270d459a7e3b1b56240688
Kirya <kiril@qw>
2022-06-14 21:24:38 +0300

    123

--- MainForm.py
+++ MainForm.py
@@ -19,13 +19,18 @@ class MyMainForm(QtWidgets.QMainWindow):
         self.allTrueSymbols = 0
         self.timerSec = QtCore.QTimer()
         self.timerSec.timeout.connect(self.onTimer)
+        self.__ui.lineEditOutputText.setReadOnly(True)
+        self.__ui.lineEditEnterText.setReadOnly(True)
 
     def Start(self):
         self.__ui.lineEditOutputText.setText('')
         self.__ui.lineEditEnterText.setText('')
         self.onEnterText('')
-        self.secValue = 80
+        self.allSymbols = 0
+        self.allTrueSymbols = 0
+        self.secValue = 5
         self.timerSec.start(1000)
+        self.__ui.lineEditEnterText.setReadOnly(False)
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Q:
@@ -35,16 +40,17 @@ class MyMainForm(QtWidgets.QMainWindow):
     def onEnterText(self, newText):
         if newText != '':
             self.allSymbols += 1
-            a = newText[len(newText)-1]
-            b = self.__ui.lineEditOutputText.text()[len(newText)-1]
-            if a != b:
-                newText = newText.removesuffix(a)
-                self.__ui.lineEditEnterText.setText(newText)
-            else:
-                self.allTrueSymbols += 1
-         #self.__ui.lineEditOutputText.setText(newText)
+            a = newText[len(newText) - 1]
+            if len(self.__ui.lineEditOutputText.text()) != 0:
+                b = self.__ui.lineEditOutputText.text()[len(newText) - 1]
+                if a != b:
+                    newText = newText.removesuffix(a)
+                    self.__ui.lineEditEnterText.setText(newText)
+                else:
+                    self.allTrueSymbols += 1
+        # self.__ui.lineEditOutputText.setText(newText)
         if self.__ui.lineEditEnterText.text() == self.__ui.lineEditOutputText.text():
-            #print('y')
+            # print('y')
             if (len(self.dict) > 0):
                 idx = random.randrange(0, len(self.dict))
                 self.__ui.lineEditEnterText.setText('')
@@ -71,8 +77,8 @@ class MyMainForm(QtWidgets.QMainWindow):
             self.timerSec.stop()
             msgBox = QtWidgets.QMessageBox()
             msgBox.setIcon(QtWidgets.QMessageBox.Information)
-            msgBox.setText("Натыкано правильных знаков: "+str(self.allTrueSymbols)+"\n"+
-                           "Натыкано не правильных знаков: "+str(self.allSymbols)+"\n")
+            msgBox.setText("Натыкано правильных знаков: " + str(self.allTrueSymbols) + "\n" +
+                           f"Натыкано не правильных знаков: {self.allSymbols - self.allTrueSymbols}\n")
             msgBox.setWindowTitle("Время вышло")
             msgBox.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
 
